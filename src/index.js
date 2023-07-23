68cf4f4a8937c5d94e0b5d2ac5fb939f27017b9e
Garik Bagdasaryan <bagdasaryan.garik.21@gmail.com>
2023-07-23 11:57:42 +0300

    FontsFix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import './fonts/Montserrat/Montserrat-Regular.ttf';
-import './fonts/Gotham/gotham_bold.otf';
 import App from './App';
 
 
