567d414dbaeabe8f3f02bf99460a99bf6c2b8c39
Alexei Doloban <leha.doloban@yandex.ru>
2023-07-23 12:50:15 +0300

    WaitChages

--- src/App.js
+++ src/App.js
@@ -221,7 +221,7 @@ function App() {
           </div>
           <div className="Footer-slice__copyright">
             <div className="Footer-slice__copyright__text">
-              © 2023. Все права защищены. Создано с ❤️ от Севро
+              © 2023. Все права защищены. Создано с ❤️ от Севроо
             </div>
           </div>
         </div>
