0357fccdf8ae2d999bac2e693e29a41bfad238a7
Garik Bagdasaryan <bagdasaryan.garik.21@gmail.com>
2023-07-23 22:57:26 +0300

    SliderFix

--- src/App.js
+++ src/App.js
@@ -185,15 +185,18 @@ function App() {
                       </clipPath>
                     </defs>
                   </svg>
-
                 </div>
               </div>
               <div className="News-slice__slider__header-arrows">
                 <button className="News-slice__slider__header-arrows_left">
-                <img src="./img/arrow-right.png"></img>
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="22" viewBox="0 0 12 22" fill="none">
+<path d="M11.28 0.800102C10.776 0.344102 10.752 0.368102 5.448 5.6721L0.119995 11.0001L5.448 16.3281C10.752 21.6321 10.776 21.6561 11.28 21.2001L11.784 20.7441L6.912 15.8721L2.04 11.0001L6.912 6.1281L11.784 1.2561L11.28 0.800102Z" fill="white"/>
+</svg>
                 </button>
                 <button className="News-slice__slider__header-arrows_right">
-                <img src="./img/arrow-right.png"></img>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<path d="M6.72 22.1999C7.224 22.6559 7.248 22.6319 12.552 17.3279L17.88 11.9999L12.552 6.6719C7.248 1.3679 7.224 1.3439 6.72 1.7999L6.216 2.2559L11.088 7.1279L15.96 11.9999L11.088 16.8719L6.216 21.7439L6.72 22.1999Z" fill="white"/>
+</svg>
                 </button>
               </div>
             </div>
