5d1eee52abd81c37e2f59894e914e6526136245f
Garik Bagdasaryan <bagdasaryan.garik.21@gmail.com>
2023-07-23 12:37:53 +0300

    FontsFix2

--- src/App.js
+++ src/App.js
@@ -47,19 +47,19 @@ function App() {
         <div className="container Statistics-slice-container">
           <div className="Statistics-slice__directions">
             <div className="Statistics-slice__directions_text">направлений</div>
-            <div className="Statistics-slice__directions_cout">6</div>
+            <div className="Statistics-slice__directions_cout Statistics-slice__title">6</div>
           </div>
           <div className="Statistics-slice__detachment">
             <div className="Statistics-slice__detachment_text">отрядов</div>
-            <div className="Statistics-slice__detachment_cout">20+</div>
+            <div className="Statistics-slice__detachment_cout Statistics-slice__title">20+</div>
           </div>
           <div className="Statistics-slice__fighters">
             <div className="Statistics-slice__fighters_text">бойцов</div>
-            <div className="Statistics-slice__fighters_cout">1000+</div>
+            <div className="Statistics-slice__fighters_cout Statistics-slice__title">1000+</div>
           </div>
           <div className="Statistics-slice__years">
             <div className="Statistics-slice__years_text">лет</div>
-            <div className="Statistics-slice__years_cout">8</div>
+            <div className="Statistics-slice__years_cout Statistics-slice__title">8</div>
           </div>
         </div>
       </div>
