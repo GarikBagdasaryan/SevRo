914ceaa327bc1d57aa52a2156a44ec152b07e21e
Garik Bagdasaryan <bagdasaryan.garik.21@gmail.com>
2023-07-24 23:13:01 +0300

    MakeComponents

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,72 @@
+function Footer() {
+	return (
+		<div className="Footer-slice">
+			<div className="container">
+				<div className="Footer-slice__content">
+					<div className="Footer-slice__content__links">
+						навигация
+						<div className="Footer-slice__content__links__nav">
+							<ul>
+								<li>
+									<a href="">о нас</a>
+								</li>
+								<li>
+									<a href="">деятельность</a>
+								</li>
+								<li>
+									<a href="">новости</a>
+								</li>
+								<li>
+									<a href="">галерея</a>
+								</li>
+								<li>
+									<a href="">другое</a>
+								</li>
+								<li>
+									<a href="">Q/A</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="Footer-slice__content__contact">
+						контакты
+						<div className="Footer-slice__content__contact_nav">
+							<ul>
+								<li>
+									<a href="">вконтакте</a>
+								</li>
+								<li>
+									<a href="">телеграм</a>
+								</li>
+								<li>
+									<a href="">+79780002255</a>
+								</li>
+								<li>
+									<a href="">sevro@mai.ru</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="Footer-slice__content__info">
+						<div className="Footer-slice__content__info__name">
+							Найди работу в студенческих отрядах
+						</div>
+						<div className="Footer-slice__content__info__button">
+							<button className="Footer-slice__content__info__button-style">
+								Вступить
+								<img src="./img/arrow-right.png"></img>
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="Footer-slice__copyright">
+					<div className="Footer-slice__copyright__text">
+						© 2023. Все права защищены. Создано с ❤️ от Севро
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
---
+++ src/components/News-slice/NewsSliceCard.js
@@ -0,0 +1,22 @@
+function NewsSliceCard() {
+	return (
+		<div className="News-slice__slider__card">
+			<div className="News-slice__slider__card__photo">
+				<img src="./img/new__photo.png"></img>
+			</div>
+			<div className="News-slice__slider__card__text">
+				<div className="News-slice__slider__card__text__title">
+					{" "}
+					8 лет СевРо
+				</div>
+				<div className="News-slice__slider__card__text__description">
+					Студенческие педагогические отряды (СПО) – ежегодно
+					организовывают каникулярный отдых детей организовывают
+					каникулярный отдых детей каникулярный отдых детей
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default NewsSliceCard;
