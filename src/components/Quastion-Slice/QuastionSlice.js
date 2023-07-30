eaf311545547feab425805d0c8b741dc457fd89b
Alexei Doloban <leha.doloban@yandex.ru>
2023-07-30 13:27:13 +0300

    addquastions

---
+++ src/components/Quastion-Slice/QuastionSlice.js
@@ -0,0 +1,22 @@
+function QuastionSlice() {
+    return (
+        <div className="Quastion-slice">
+            <div className="container Quastion-slice__blocks">
+                <details className="Quastion-slice__block">
+                    <summary className="Quastion-slice__block__title" >Что такое «киновселенная»?</summary>
+                    <p className="Quastion-slice__block__text">Действие всех фильмов Marvel происходит в одной вселенной. То есть события одного фильма могут напрямую влиять на сюжет другого, а персонажи свободно перемещаются между фильмами. С недавних пор киновселенная включает в себя и несколько сериалов.</p>
+                </details>
+                <details className="Quastion-slice__block">
+                    <summary className="Quastion-slice__block__title">Что такое «Мстители»?</summary>
+                    <p className="Quastion-slice__block__text">Мстители — команда супергероев Marvel. Первый комикс о «Мстителях» вышел ещё в 1963 году и был ответом Marvel на успешную серию комиксов DC о «Лиге справедливости» во главе с Бэтменом и Суперменом. В оригинальном комиксе в состав первых Мстителей входили Железный Человек, Тор, Халк, а также Человек-Муравей и Оса.</p>
+                </details>
+                <details className="Quastion-slice__block">
+                    <summary className="Quastion-slice__block__title">Так, а почему весь зал оживляется, когда в кадре появляется какой-то седой мужик в очках?</summary>
+                    <p className="Quastion-slice__block__text">Потому что это — Стэн Ли, создавший половину этих супергероев. Сейчас он появляется в каждом фильме про героев комиксов Marvel в маленькой эпизодической роли. Впервые это случилось в первом большом блокбастере по комиксам Marvel: в «Людях Икс» в 2000 году. Стэн умер в 2018 году. «Мстители: Финал» стал последним фильмом с его участием.</p>
+                </details>
+            </div>
+        </div>
+    );
+}
+
+export default QuastionSlice;
