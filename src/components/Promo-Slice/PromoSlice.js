336731afdb48538080f04ec9857530d78bb53a40
Garik Bagdasaryan <bagdasaryan.garik.21@gmail.com>
2023-08-13 19:51:57 +0300

    PeredSmertbu

--- src/components/Promo-Slice/PromoSlice.js
+++ src/components/Promo-Slice/PromoSlice.js
@@ -1,25 +1,23 @@
-
 import Header from "../Header/Header";
-import '../Promo-Slice/promo.scss';
+import "../Promo-Slice/promo.scss";
 
 function PromoSlice() {
-    return (
-        < div className="Promo-slice" >
-
-            <Header />
-            <div className="container ">
-                <div className="Promo-slice__info ">
-                    Найди работу в студенческих отрядах
-                </div>
-                <div className="Promo-slice__button">
-                    <button className="Promo-slice__button__stylebtn">
-                        Вступить
-                        <img src="./img/arrow-right.png"></img>
-                    </button>
-                </div>
-            </div>
-        </div >
-    );
+	return (
+		<div className="Promo-slice">
+			<Header />
+			<div className="container ">
+				<div className="Promo-slice__info ">
+					Найди работу в студенческих отрядах
+				</div>
+				<div className="Promo-slice__button">
+					<button className="Promo-slice__button__stylebtn">
+						Вступить
+						<img src="./img/arrow-right.png"></img>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default PromoSlice;
--- src/pages/News-page/Newspage.js
+++ src/pages/News-page/Newspage.js
@@ -1,11 +1,10 @@
-
 import Header from "../../components/Header/Header";
 import NewsSlice from "../News-page/News-slice/NewsSlice";
 
 function Newspage() {
 	return (
 		<div className="Newspage">
-			<Header />
+			<Header NavDark="test" />
 			<NewsSlice />
 		</div>
 	);
