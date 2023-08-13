65fa51babd8f6d9d861a54f55dac32ab0cf5b410
Alexei Doloban <alekdn.work@yandex.ru>
2023-08-13 23:59:01 +0300

    push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,11 @@ function Header() {
 	const { pathname, hash, key } = useLocation();
 
 	useEffect(() => {
-		// if not a hash link, scroll to top
+
 		if (hash === '') {
 			window.scrollTo(0, 0);
 		}
-		// else scroll to id
+
 		else {
 			setTimeout(() => {
 				const id = hash.replace('#', '');
