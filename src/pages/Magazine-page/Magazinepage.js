b6221398b1a148a4ec18a74fc032d7d94ab7ed4e
Garik Bagdasaryan <bagdasaryan.garik.21@gmail.com>
2023-08-15 13:54:55 +0300

    FixBEM

--- src/pages/Magazine-page/Magazinepage.js
+++ src/pages/Magazine-page/Magazinepage.js
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
+import DirectionsSlice from "../../components/Directions-slice/DirectionsSlice";
 import NewsSlice from "../../components/MagazineCard-slice/MagazineCard";
 
 function Newspage() {
 	return (
 		<div className="Newspage">
 			<Header />
+			<DirectionsSlice />
 			<NewsSlice />
 		</div>
 	);
