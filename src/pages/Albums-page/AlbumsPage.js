eca0f536acf6d212ded09df7d5722180ffc4ca98
Alexei Doloban <alekdn.work@yandex.ru>
2023-08-17 22:42:20 +0300

    push gallery

---
+++ src/pages/Albums-page/AlbumsPage.js
@@ -0,0 +1,12 @@
+import GallerySlice from "../../components/Gallery-slice/GallerySlice";
+
+
+function AlbumsPage() {
+    return (
+        <div className="AlbumsPage">
+            <GallerySlice />
+        </div>
+    );
+}
+
+export default AlbumsPage;
