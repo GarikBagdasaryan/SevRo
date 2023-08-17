cade848495442c0738b3553c6b333baf77155a7f
Garik Bagdasaryan <bagdasaryan.garik.21@gmail.com>
2023-08-17 15:36:50 +0300

    AddSquadCard

---
+++ src/components/SquadCard/SquadCard.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function SquadCard() {
+	return (
+		<div className="squad-card">
+			<img src="./img/squad_SL.jpg" className="squad-card__img" />
+			<div className="squad-card__name">Стальные Львы</div>
+		</div>
+	);
+}
+
+export default SquadCard;
---
+++ src/pages/Direction-page/DirectionPage.js
@@ -0,0 +1,12 @@
+import SquadCard from "../../components/SquadCard/SquadCard";
+import "./directionpage.scss";
+
+function DirectionPage() {
+	return (
+		<div className="DirectionPage">
+			<SquadCard />
+		</div>
+	);
+}
+
+export default DirectionPage;
