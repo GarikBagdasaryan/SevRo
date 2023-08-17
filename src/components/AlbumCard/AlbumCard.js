13971301df875315e5e525cb606217bc2bdb0750
Alexei Doloban <alekdn.work@yandex.ru>
2023-08-18 00:45:13 +0300

    push grid

--- src/components/AlbumCard/AlbumCard.js
+++ src/components/AlbumCard/AlbumCard.js
@@ -2,10 +2,14 @@ import '../AlbumCard/AlbumCard.scss';
 
 function AlbumCard() {
 
+    var styles = {
+        Grid: {
 
+        }
+    };
 
     return (
-        <div className="AlbumCard">
+        <div className="AlbumCard" style={styles.Grid}>
             <img src="./img/people__card.png" className="AlbumCard__photo"></img>
             <div className="AlbumCard__title">
                 Закрытие ТТС 2023
--- src/components/Gallery-slice/GallerySlice.js
+++ src/components/Gallery-slice/GallerySlice.js
@@ -3,16 +3,34 @@ import "../Gallery-slice/GallerySlice.scss";
 
 
 function GallerySlice() {
+
+	const ActionGallery = event => {
+
+
+		let htmlElements =
+			document.getElementsByClassName("AlbumCard__photo");
+		console.log(htmlElements.length);
+		for (let i = 0; i < htmlElements.length; i = i + 5) {
+			htmlElements[i].style.width = "580px";
+			htmlElements[i].style.height = "580px";
+		}
+	}
 	return (
 		<div className="Gallery-slice">
 			<div className="container Gallery-slice-container">
 				<div className="Gallery-slice__title">галерея</div>
-				<div className="Gallery-slice__albums">
+				<div className="Gallery-slice__albums" onLoad={ActionGallery}>
 					<AlbumCard />
 					<AlbumCard />
 					<AlbumCard />
 					<AlbumCard />
 					<AlbumCard />
+					<AlbumCard />
+					<AlbumCard />
+					<AlbumCard />
+					<AlbumCard />
+					<AlbumCard />
+
 				</div>
 
 			</div>
