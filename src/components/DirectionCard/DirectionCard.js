6a0b3cbbec0238e2405658789974b7a8155ba161
Alexei Doloban <alekdn.work@yandex.ru>
2023-08-17 15:49:31 +0300

    add component directioncard

---
+++ src/components/DirectionCard/DirectionCard.js
@@ -0,0 +1,15 @@
+import '../DirectionCard/DirectionCard.scss';
+
+function DirectionCard() {
+    return (
+
+        <div className="Directions-slice__icons__card">
+                        <img className="Directions-slice__icons__card__style" src="img/directions2.jpg"></img>
+                        <div className="Directions-slice__icons__card__overlay">
+                            <img className="Directions-slice__icons__card__overlay__style" src="img/ico11-spo.svg"></img>
+                        </div>
+                    </div>
+    );
+}
+
+export default DirectionCard;
