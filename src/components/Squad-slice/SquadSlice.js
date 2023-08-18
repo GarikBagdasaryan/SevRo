87d943eaa7aec8e6a13962ef894ff90eda0deb04
Alexei Doloban <alekdn.work@yandex.ru>
2023-08-18 13:55:31 +0300

    Winner gallery

--- src/components/Squad-slice/SquadSlice.js
+++ src/components/Squad-slice/SquadSlice.js
@@ -3,19 +3,7 @@ import SquadCard from "../SquadCard/SquadCard";
 
 
 function SquadSlice() {
-	const LoadImg = (event) => {
-		event.currentTarget.classList.add("squad-card_active");
-	};
-	const ActionImg = (event) => {
-		const cards = document.querySelectorAll(".squad-card");
-		for (let i in cards) {
-			if (cards[i].classList.contains("squad-card_active")) {
-				cards[i].classList.remove("squad-card_active");
-				break;
-			}
-		}
-		event.currentTarget.classList.add("squad-card_active");
-	};
+
 	return (
 		<div className="Squad-slice">
 			<div className="container">
